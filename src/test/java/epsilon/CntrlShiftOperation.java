4398f99e43c8469bab964739835f86878193781b
Kumar <kumar.ajeet@siemens-healthineers.com>
2021-11-03 10:01:26 +0530

    added some code

--- src/test/java/epsilon/CntrlShiftOperation.java
+++ src/test/java/epsilon/CntrlShiftOperation.java
@@ -4,37 +4,43 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FindAllLinks 
+
+public class CntrlShiftOperation 
 {
 static WebDriver driver;
 
   @Test
-  public void f()
+  public void controlShift()
   {
-	  List<WebElement> lst=driver.findElements(By.xpath("//a"));
-	  for(int i=0;i<lst.size();i++)
-	  {
-		String links=lst.get(i).getText();
-		System.out.println(links);
-	  }
+	    driver.findElement(By.id("email")).click();
+	    System.out.println("click done");
+		Actions act = new Actions(driver);
+		act.keyDown(Keys.SHIFT).sendKeys("a");
+		act.keyUp(Keys.SHIFT).build().perform();
+		act.keyDown(Keys.CONTROL).sendKeys("a");
+		act.keyUp(Keys.CONTROL).build().perform();
+		driver.findElement(By.id("pass")).click();
+		act.keyDown(Keys.CONTROL).sendKeys("v");
+		act.keyUp(Keys.SHIFT).build().perform();
   }
   @BeforeMethod
   public void setUp() 
   {
 	    System.setProperty("webdriver.gecko.driver", "C:\\Users\\z00467ka\\Desktop\\Selenium files\\geckodriver-v0.29.1-win64\\geckodriver.exe");	
-	    driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://www.javatpoint.com/how-to-print-ascii-value-in-java");
+		driver.get("https://www.facebook.com/");
   }
   @AfterMethod
   public void tearDown() 
@@ -42,3 +48,4 @@ static WebDriver driver;
 	  driver.close();
   }
 }
+
--- src/test/java/epsilon/WelcomeToCompany.java
+++ src/test/java/epsilon/WelcomeToCompany.java
@@ -8,18 +8,21 @@ public class WelcomeToCompany
 		StringBuffer sb = new StringBuffer();
 		for(int i=0;i<arr.length;i++)
 		{
-			if(arr[i]>='a' && arr[i]<='z' || arr[i]==' ')
+			if(arr[i]>='a' && arr[i]<='z' || arr[i]>='A' && arr[i]<='Z' || arr[i]==' ')
 			{
+				if(arr[i]==' ')
+				{
+					arr[i+1]=(char)(arr[i+1]-32);
+				}
 				sb.append(arr[i]);
 			}
 		}
-		return sb.toString();
-		
+		return sb.toString();	
 	}
 
 	public static void main(String[] args) 
 	{
-		String s ="welcome to {company}";
+		String s ="Welcome to {Company}";
 		System.out.println(expword(s));
 		
 
